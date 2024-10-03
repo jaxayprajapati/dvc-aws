3f577b097c88a91835bbf88bde33737bd8514fcb
jaxayprajapati <jaxayprajapati@gmail.com>
2024-10-03 06:06:49 +0530

    DVC live experiment tracking done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -13,23 +13,6 @@ logger_obj = Logger(logger_file_name=LOG_FILE_NAME, log_directory=LOG_DIRECTORY_
 logger = logger_obj.__logger__
 
 
-def load_params(params_path: str) -> dict:
-    """Load parameters from a YAML file."""
-    try:
-        with open(params_path, 'r') as file:
-            params = yaml.safe_load(file)
-        logger.debug('Parameters retrieved from %s', params_path)
-        return params
-    except FileNotFoundError:
-        logger.error('File not found: %s', params_path)
-        raise
-    except yaml.YAMLError as e:
-        logger.error('YAML error: %s', e)
-        raise
-    except Exception as e:
-        logger.error('Unexpected error: %s', e)
-        raise
-
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
